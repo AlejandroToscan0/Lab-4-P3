6d17c9985d6b1eb4dbf115709e88637dcba85a75
AlejandroToscan0 <jatoscano6@espe.edu.ec>
2025-08-07 08:48:30 -0500

    3

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,8 @@
-const test = require('node:test');
+const test = require('test');
 const suma = require('./sum');
 
+// sum.test.js
+
 test('suma 1 + 2 debe dar 3', () => {
-    if (suma(1, 2) !== 3)
-        expect(sum(1, 2)).toBe(3);
+    expect(suma(1, 2)).toBe(3);
 });
